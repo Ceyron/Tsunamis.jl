bbb610419b8e8e275bd331a5a77e9c6993f2304d
Felix Köhler <f.koehler@tum.de>
2020-05-19 21:07:31 +0200

    Add data structures for fluxes and create a stub of the main simulation routine

--- single_node.jl
+++ single_node.jl
@@ -2,10 +2,19 @@
    Contains a single-node sequential implementation of an SWE solver for the simulation of tsunamis
 =#
 
-import Plots
-Plots.plotlyjs()
+#import Plots
+using Printf
+
+# Use Plotly backend in an electron chromium window (nice visuals :D)
+#Plots.plotlyjs()
 
 include("src/CartesianBlock.jl")
+
+struct Time_Mesh
+    time_end::Float64
+    num_checkpoints::Float64
+    time_nodes::Array{Float64, 1}
+end
 
 # The main simulation data structure
 #
@@ -14,12 +23,16 @@ include("src/CartesianBlock.jl")
 # simulation
 mutable struct SWE_Simulation
     current::Simulation_Block
-    checkpoints::Array{SWE_Fields, 1}
+    checkpoint_fields::Array{SWE_Fields, 1}
+    time_mesh::Time_Mesh
 end
 
-
-const size_x = 50;
-const size_y = 50;
+# Some stub constants for the simulation, has to be set by command-line options
+# later on
+const size_x = 5000;
+const size_y = 5000;
+const num_cells_x = 50;
+const num_cells_y = 50;
 const time_end = 15;
 const num_checkpoints = 20;
 
@@ -29,40 +42,126 @@ function main()
     # Instantiate main block
     simulation_single_node = SWE_Simulation(
         Simulation_Block(
+            Layout(
+                # No offset since it is single block in a sequential implementation
+                0.0,
+                0.0,
+                # Set the size of the simulation domain (halo cells do not count
+                # towards this)
+                size_x,
+                size_y,
+                # Set number of interior cells
+                num_cells_x,
+                num_cells_y,
+                # Calculate and set the cell widths
+                size_x / num_cells_x,
+                size_y /num_cells_y,
+            ),
             SWE_Fields(
                 # Account for the additional halo layers of the boundary
-                zeros(size_x+2, size_y+2),
-                zeros(size_x+2, size_y+2),
-                zeros(size_x+2, size_y+2),
+                zeros(num_cells_x + 2, num_cells_y + 2),
+                zeros(num_cells_x + 2, num_cells_y + 2),
+                zeros(num_cells_x + 2, num_cells_y + 2),
             ),
-            zeros(size_x+2, size_y+2),
+            # Set initial time to zero
+            0.0,
+            # The Bathymetry data
+            zeros(num_cells_x + 2, num_cells_y + 2),
             # No particular distribution occurs in the single_node case,
             # therefore set all boundary conditions to periodic
-            Boundary(
-                periodic,
-                "none"
-            ),
-            Boundary(
-                periodic,
-                "none"
-            ),
-            Boundary(
-                periodic,
-                "none"
-            ),
-            Boundary(
-                periodic,
-                "none"
+            Boundary_Collection(
+                Boundary(
+                    periodic,
+                    "none"
+                ),
+                Boundary(
+                    periodic,
+                    "none"
+                ),
+                Boundary(
+                    periodic,
+                    "none"
+                ),
+                Boundary(
+                    periodic,
+                    "none"
+                ),
             ),
         ),
+        # Preallocate the space required to save all 3 fields at every
+        # checkpoint
         Array{SWE_Fields, 1}(undef, num_checkpoints),
+        # Contains information on the time integration
+        Time_Mesh(
+            time_end,
+            num_checkpoints,
+            range(0, time_end; length=num_checkpoints),
+        ),
     )
 
     # Imprint the initial condition
     simulation_single_node.current.fields.h[22:28, 22:28] .= 3.0
 
-    display(Plots.surface(1:52, 1:52, simulation_single_node.current.fields.h))
-    readline()
+    # Copy the initial state into the checkpoints state for reference
+    simulation_single_node.checkpoint_fields[1] = deepcopy(
+        simulation_single_node.current.fields
+    )
+
+    # Print the head
+    println("Welcome to SWE solver on julia")
+    println("time|todo")
+
+    # Instantiate the flux field struct to be used over the iterations
+    fluxes = Fluxes(
+        Flux_Field(
+            zeros(num_cells_x, num_cells_y),
+            zeros(num_cells_x, num_cells_y),
+            zeros(num_cells_x, num_cells_y),
+            zeros(num_cells_x, num_cells_y),
+        ),
+        Flux_Field(
+            zeros(num_cells_x, num_cells_y),
+            zeros(num_cells_x, num_cells_y),
+            zeros(num_cells_x, num_cells_y),
+            zeros(num_cells_x, num_cells_y),
+        ),
+        Flux_Field(
+            zeros(num_cells_x, num_cells_y),
+            zeros(num_cells_x, num_cells_y),
+            zeros(num_cells_x, num_cells_y),
+            zeros(num_cells_x, num_cells_y),
+        ),
+        # The max wave speed standard value
+        0.0,
+    )
+
+    # Iterate over all checkpoints
+    for i_checkpoint in 2:num_checkpoints
+
+
+        # Integrate until next checkpoint is reached
+        while simulation_single_node.current.time <
+                simulation_single_node.time_mesh.time_nodes[i_checkpoint]
+            println("$(simulation_single_node.current.time)")
+            # (1) set values in ghost layer
+
+            # (2) Compute numerical fluxes
+
+            # (3) Calculate the new time step (maximum allows time_step due to
+            # the wave speeds)
+            time_step = 0.5
+
+            # (4) Update the cell values Gudonov style first order
+
+            # (5) Refresh to current simulation time
+            simulation_single_node.current.time += time_step
+            
+        end
+    end
+
+
+
+
 
 end
 
--- src/CartesianBlock.jl
+++ src/CartesianBlock.jl
@@ -3,11 +3,13 @@ Implements the main data structure and all routines of the main simulation loop
 """
 
 
+mutable struct SWE_Fields
 # Contains the 3 conserved field variables (that depend on 2D-space and time) as
 # well as the bathymetry (this also helps to represent the version of the SWE
 # that can work without source terms -> transferring the source terms into a
 # separate variable in the hyperbolic equation)
-mutable struct SWE_Fields
+#
+# The simulation domain is 2d with halo cells around to support for boundaries and decomposed synchronization. 
     h::Array{Float64, 2}
     hu::Array{Float64, 2}
     hv::Array{Float64, 2}
@@ -23,11 +25,56 @@ struct Boundary
     connector::String
 end
 
-mutable struct Simulation_Block
-    fields::SWE_Fields
-    bathymetry::Array{Float64, 2}
+# Contains all the 4 edges of a Cartesian Block
+struct Boundary_Collection
     left::Boundary
     top::Boundary
     right::Boundary
     bottom::Boundary
+end
+
+# Defines the structure of a simulation Block
+#
+#
+#
+#
+#
+#
+#  ^y
+#  |
+#  --> x
+struct Layout
+    offset_x::Float64
+    offset_y::Float64
+    size_x::Float64
+    size_y::Float64
+    num_interior_cells_x::UInt64
+    num_interior_cells_y::UInt64
+    cell_width_x::Float64
+    cell_width_y::Float64
+end
+
+mutable struct Simulation_Block
+    layout::Layout
+    fields::SWE_Fields
+    time::Float64
+    bathymetry::Array{Float64, 2}
+    boundaries::Boundary_Collection
+end
+
+# Contains 4 arrays of the same size as the simulation domain that contain the
+# numerical fluxes at the respective edges
+mutable struct Flux_Field
+    left::Array{Float64, 2}
+    top::Array{Float64, 2}
+    right::Array{Float64, 2}
+    bottom::Array{Float64, 2}
+end
+
+# Contains all the fluxes for a simulation block
+mutable struct Fluxes
+    h::Flux_Field
+    hu::Flux_Field
+    hv::Flux_Field
+    max_wave_speed::Float64
 end
